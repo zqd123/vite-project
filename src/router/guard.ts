e415b19da2caf7ecd41b691b4e2c67b81cc6dd79
zhangqd <zhangqd@zhangqddeMacBook-Air.local>
2023-04-11 14:09:56 +0800

    feat: 添加Nprogress页面加载进度条

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -1,11 +1,15 @@
 import router from './index'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css';
 
 /**前置守卫 */
 router.beforeEach((to,from)=>{
     console.log({to,from});
+    nprogress.start()
     return true;
 })
 /**后置守卫 */
 router.afterEach((to,from)=>{
+    nprogress.done()
     // console.log({to,from});
 })
