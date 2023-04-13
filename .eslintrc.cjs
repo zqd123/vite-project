bf9d7cf0135c21aeea0f6d9d368b235a151e955a
zhangqd <zhangqd@zhangqddeMacBook-Air.local>
2023-04-13 18:30:06 +0800

    feat: eslint进行中。。。

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+      //TODO
+      "plugin:vue/vue3-essential",
+      'plugin:vue/vue3-recommended',
+    ],
+    rules: {
+      // override/add rules settings here, such as:
+      'vue/no-unused-vars': 'error'
+    }
+  }
