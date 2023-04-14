0f06a005d22cc525dbe149e643ee56d3f52db25a
zhangqd <zhangqd@zhangqddeMacBook-Air.local>
2023-04-14 16:02:41 +0800

    feat: eslint暂告一段落

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,28 @@
 module.exports = {
-    extends: [
-      //TODO
-      "plugin:vue/vue3-essential",
-      'plugin:vue/vue3-recommended',
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        'plugin:vue/vue3-recommended',
+        "plugin:@typescript-eslint/recommended"
     ],
-    rules: {
-      // override/add rules settings here, such as:
-      'vue/no-unused-vars': 'error'
+    "overrides": [
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser",
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "vue/multi-word-component-names": "off",
+        "@typescript-eslint/no-unused-vars": "warn"
     }
-  }
+}
