c0c8e4a69fe4973578884e9e4cc0794ba91f4ca1
zhangqd <zhangqd@zhangqddeMacBook-Air.local>
2023-04-14 18:04:50 +0800

    fix: :ambulance:解决Prettier和Eslint冲

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:vue/vue3-recommended",
     "plugin:@typescript-eslint/recommended",
+    "prettier"
   ],
   overrides: [],
   parser: "vue-eslint-parser",
