8fc868262115a525eb09dad061b5c7fdaabac0b0
zhangqd <zhangqd@zhangqddeMacBook-Air.local>
2023-04-14 16:47:34 +0800

    feat: 添加prettier格式化代码-初次提交

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        'plugin:vue/vue3-recommended',
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "vue-eslint-parser",
-    "parserOptions": {
-        "parser": "@typescript-eslint/parser",
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "vue/multi-word-component-names": "off",
-        "@typescript-eslint/no-unused-vars": "warn"
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+};
