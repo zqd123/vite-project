11c44106b019c3cc1932adb2c62751101677484f
zqd123 <35557054+zqd123@users.noreply.github.com>
2023-04-27 07:40:34 +0000

    Deploying to page-ci from @ zqd123/vite-project@f52b16477fcc94bdbe2c1c7923d8d6243166815e 🚀

---
+++ assets/index-4f596866.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as n,c as t,a as c}from"./index-925e5ead.js";const s={};function a(_,i){const e=r("router-view");return n(),t("div",null,[c(e)])}const d=o(s,[["render",a]]);export{d as default};
