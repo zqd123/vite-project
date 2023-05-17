963273bb91d32ca3d8bce9c5e3cc79b5017c819a
zqd123 <35557054+zqd123@users.noreply.github.com>
2023-05-17 12:42:47 +0000

    Deploying to page-ci from @ zqd123/vite-project@18dee236ef8b6642d0b87860549f3ebe567b622c 🚀

---
+++ assets/OverPage-520e6328.js
@@ -0,0 +1,1 @@
+import{_ as t,o as s,l as c,m as e}from"./index-be117407.js";const o={},n={class:"flex flex-col justify-center items-center"},_=e("div",null,"完成!",-1),r=e("div",null,"感谢您的合作!",-1),a=[_,r];function l(i,d){return s(),c("div",n,a)}const m=t(o,[["render",l]]);export{m as default};
